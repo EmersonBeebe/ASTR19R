14f8b1b7b15b51aa34ed9c31aafd0e9ae93a039d
EmersonBeebe <ecbeebe2023@gmail.com>
2024-10-17 19:16:06 -0700

    Update journalOne.py
    huh

--- journalOne.py
+++ journalOne.py
@@ -1,1 +1,4 @@
-print("My name is Emerson Beebe.\nMy pronouns are he and him.\nMy favorite food is sushi.\nMy favorite movie is Interstellar.")
+def main():
+  print("My name is Emerson Beebe.\nMy pronouns are he and him.\nMy favorite food is sushi.\nMy favorite movie is Interstellar.")
+if __name__ == "__main__":
+  main()
